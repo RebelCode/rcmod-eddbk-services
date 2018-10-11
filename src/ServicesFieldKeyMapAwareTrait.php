0244f014597b5e86055f1d1399e2b7e2025b776e
Miguel Muscat <miguelmuscat93@gmail.com>
2018-10-11 10:27:24 +0200

    Added services field-kep map awareness trait

---
+++ src/ServicesFieldKeyMapAwareTrait.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace RebelCode\EddBookings\Services;
+
+/**
+ * Provides awareness of the services field-key map.
+ *
+ * @since [*next-version*]
+ */
+trait ServicesFieldKeyMapAwareTrait
+{
+    /**
+     * Retrieves the field-key map for services.
+     *
+     * @since [*next-version*]
+     *
+     * @return array
+     */
+    protected function _getServicesFieldKeyMap()
+    {
+        return [
+            // Core service fields
+            'id'               => 'ID',
+            'name'             => 'post_title',
+            'description'      => 'post_excerpt',
+            'status'           => 'post_status',
+            'image_id'         => 'image_id',
+            // Other post fields
+            'guid'             => 'guid',
+            'post_author'      => 'post_author',
+            'type'             => 'post_type',
+            'date'             => 'post_date',
+            'date_gmt'         => 'post_date_gmt',
+            'content'          => 'post_content',
+            'content_filtered' => 'post_content_filtered',
+            'category'         => 'post_category',
+            'password'         => 'post_password',
+            'parent'           => 'post_parent',
+            'modified'         => 'post_modified',
+            'modified_gmt'     => 'post_modified_gmt',
+            'mime_type'        => 'post_mime_type',
+            'tags_input'       => 'tags_input',
+            'tax_input'        => 'tax_input',
+            'meta_input'       => 'meta_input',
+            'pinged'           => 'pinged',
+            'ping_status'      => 'ping_status',
+            'comment_count'    => 'comment_count',
+            'comment_status'   => 'comment_status',
+            'menu_order'       => 'menu_order',
+        ];
+    }
+}
---
+++ test/functional/ServicesFieldKeyMapAwareTraitTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace RebelCode\EddBookings\Services\FuncTest;
+
+use \InvalidArgumentException;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use Xpmock\TestCase;
+use RebelCode\EddBookings\Services\ServicesFieldKeyMapAwareTrait as TestSubject;
+
+/**
+ * Tests {@see TestSubject}.
+ *
+ * @since [*next-version*]
+ */
+class ServicesFieldKeyMapAwareTraitTest extends TestCase
+{
+    /**
+     * The class name of the test subject.
+     *
+     * @since [*next-version*]
+     */
+    const TEST_SUBJECT_CLASSNAME = 'RebelCode\EddBookings\Services\ServicesFieldKeyMapAwareTrait';
+
+    /**
+     * Creates a new instance of the test subject.
+     *
+     * @since [*next-version*]
+     *
+     * @return MockObject
+     */
+    public function createInstance()
+    {
+        // Create mock
+        $mock = $this->getMockBuilder(static::TEST_SUBJECT_CLASSNAME)
+                     ->setMethods([])
+                     ->getMockForTrait();
+
+        return $mock;
+    }
+
+    /**
+     * Tests whether a valid instance of the test subject can be created.
+     *
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createInstance();
+
+        $this->assertInternalType(
+            'object',
+            $subject,
+            'An instance of the test subject could not be created'
+        );
+    }
+
+    /**
+     * Tests the getter method to ensure correct retrieval of the field-key map.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetServicesFieldKeyMap()
+    {
+        $subject = $this->createInstance();
+        $reflect = $this->reflect($subject);
+
+        $fkm = $reflect->_getServicesFieldKeyMap();
+
+        $this->assertInternalType('array', $fkm, 'Field key map is not an array!');
+        $this->assertNotEmpty($fkm, 'Field key map is empty!');
+    }
+}
