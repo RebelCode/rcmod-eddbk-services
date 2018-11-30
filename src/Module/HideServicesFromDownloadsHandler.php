57a1c139156e2d031c370ce53625404dc8b38221
Miguel Muscat <miguelmuscat93@gmail.com>
2018-11-30 10:09:53 +0100

    Hide download query affects only list table page
    This query was affecting other pages, such as the payment list.
    This caused various download-related data to be hidden.
    
    EDDBK-178

--- src/Module/HideServicesFromDownloadsHandler.php
+++ src/Module/HideServicesFromDownloadsHandler.php
@@ -64,8 +64,6 @@ class HideServicesFromDownloadsHandler implements InvocableInterface
      */
     public function __invoke()
     {
-        global $pagenow;
-
         $event = func_get_arg(0);
 
         if (!($event instanceof EventInterface)) {
@@ -74,10 +72,10 @@ class HideServicesFromDownloadsHandler implements InvocableInterface
             );
         }
 
-        $query    = $event->getParam(0);
-        $postType = filter_input(INPUT_GET, 'post_type', FILTER_SANITIZE_STRING);
+        $query  = $event->getParam(0);
+        $screen = get_current_screen();
 
-        if (!is_admin() || $pagenow !== 'edit.php' || $postType !== $this->postType) {
+        if (!is_admin() || $screen->post_type !== $this->postType || $screen->id !== 'edit-download') {
             return;
         }
 
